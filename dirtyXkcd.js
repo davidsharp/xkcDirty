591ff9f5fc82b60653b6cb068d9daae84ab11d98
FruitJamUK <FruitJamUK@gmail.com>
2016-03-18 14:07:13 +0000

    Un-xkcd-ify-ing, for a more generic tool

--- dirtyXkcd.js
+++ dirtyXkcd.js
@@ -1,52 +1,28 @@
-//var currentXkcd = "http://xkcd.com/info.0.json?callback=";
-//var xk = "http://xkcd.com/";
-//var cd = "/info.0.json?callback=";
+var initialRequest = "http://dynamic.xkcd.com/api-0/jsonp/comic/";
 
-var xk = "http://dynamic.xkcd.com/api-0/jsonp/comic/";
-
-var comicsNo;
-var comics = [];
+var numberOfResults;
+var results = [];
 var interval;
 
-/*$.getJSON(currentXkcd, function(data){
-		comicsNo=data.num;
-		interval=setInterval(function(){if(comics.length==(comicsNo-1))printList();},5000);
-		for(var i=1;i<=comicsNo;i++){
-			if(i!==404)$.getJSON(xk+i+cd, function(moreData, i){comics.push(moreData.safe_title);});
+$.getJSON(initialRequest,
+	function(data) {
+		numberOfResults=data.num;
+		for(var i=1;i<=numberOfResults;i++){ if(i!==404){
+			$.getJSON(initialRequest+i,
+				function( moreData ) {
+					results[i]=moreData.safe_title;
+					//$('ul').append("<li>"+moreData.safe_title+"</li>");
+					if(i===numberOfResults)listCompleteCallback();
+				}
+			});}
 		}
-	});*/
-
-/*function printList(){
-	clearInterval(interval);
-	for(var j=1;j<=comics;j++){
-			$('ul').append("<li>"+comics[j]+"</li>");
-		}
-}*/
-
-
-
-$.ajax({
-url: xk,
-jsonp: "callback",
-dataType: "jsonp",
-success: function( data ) {
-
-	comicsNo=data.num;
-	//interval=setInterval(function(){if(comics.length==(comicsNo-1))printList();},5000);
-
-	for(var i=1;i<=comicsNo;i++){ if(i!==404){
-		$.ajax({
-			url: xk+i,
-			jsonp: "callback",
-			dataType: "jsonp",
-			success: function( moreData ) {
-				//comics.push(moreData.safe_title);
-				$('ul').append("<li>"+moreData.safe_title+"</li>");
-				if(i===comicsNo)alert("finished!");
-			}
-		});}
 	}
-
-
-}
 });
+
+function listCompleteCallback(){
+	$('body').append(
+			"<ul>"+
+				//TODO actually ad the stuff to this here
+			"</ul>"
+		)
+}
